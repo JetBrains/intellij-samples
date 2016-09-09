e8e1b70f9348715be93487964df8a4819604e388
hadihariri <mail@hadihariri.com>
2016-09-09 16:45:01 +0200

    Added MultiLine String

---
+++ src/main/java/com/jetbrains/editing/MultiLineStrings.java
@@ -0,0 +1,19 @@
+package com.jetbrains.editing;
+
+/**
+ * Created by hadihariri on 09/09/16.
+ */
+public class MultiLineStrings {
+
+    public void multiLineStrings() {
+
+
+        // Pressing Enter in String will auto-insert +
+        // Ctrl+Shift+J will join lines
+        String someText = "This code can be on a single line " +
+                "or it can be on multiple lines " +
+                "and you can type and hit Enter and get the + sign " +
+                "inserted automatically";
+
+    }
+}

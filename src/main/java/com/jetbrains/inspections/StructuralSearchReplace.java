4a3397f2666b24f6160ac4a24046149fdcc8a53f
hadihariri <mail@hadihariri.com>
2016-09-09 16:53:06 +0200

    Added StructuralSearchReplace Sample

---
+++ src/main/java/com/jetbrains/inspections/StructuralSearchReplace.java
@@ -0,0 +1,21 @@
+package com.jetbrains.inspections;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+/**
+ * Created by hadihariri on 09/09/16.
+ */
+public class StructuralSearchReplace {
+
+    public void emptyTryCatchBlock() throws FileNotFoundException {
+        BufferedReader reader = new BufferedReader(new FileReader("input"));
+        try {
+            reader.read();
+        } catch (Exception e) {
+            // Do nothing.
+        }
+    }
+
+}

96684e780ddfb1d2e97d8c733e7c98ccafb3bb08
hadihariri <mail@hadihariri.com>
2016-09-09 14:51:10 +0200

    Added HippieCompletion

---
+++ src/main/java/com/jetbrains/completion/HippieCompletion.java
@@ -0,0 +1,16 @@
+package com.jetbrains.completion;
+
+/**
+ * Created by hadihariri on 09/09/16.
+ */
+
+
+public class HippieCompletion {
+
+    String data1 = "Textual Input";
+    String data2 = "Text Input";
+    String data3 = "Texting Input";
+    public void hipperCompletion() {
+
+    }
+}

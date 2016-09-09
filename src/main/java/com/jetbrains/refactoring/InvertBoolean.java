6341b172ed1e338497fb92a1145af71ea5ef44f3
hadihariri <mail@hadihariri.com>
2016-09-09 17:16:37 +0200

    Added Invert Boolean Refactoring

---
+++ src/main/java/com/jetbrains/refactoring/InvertBoolean.java
@@ -0,0 +1,22 @@
+package com.jetbrains.refactoring;
+
+/**
+ * Created by hadihariri on 09/09/16.
+ */
+public class InvertBoolean {
+
+    private boolean isValid = false;
+
+
+    public void usingBooleans() {
+
+        if (!isValid) {
+            System.out.println("Something takes place here");
+        } else {
+            System.out.println("Something else takes place here");
+        }
+    }
+
+
+
+}

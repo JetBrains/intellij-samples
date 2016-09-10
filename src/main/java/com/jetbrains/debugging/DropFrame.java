3cf258da62cbee917efe55f8b83efc38c4408f0b
hadihariri <mail@hadihariri.com>
2016-09-10 08:40:08 +0200

    Added Drop Frame feature

---
+++ src/main/java/com/jetbrains/debugging/DropFrame.java
@@ -0,0 +1,37 @@
+package com.jetbrains.debugging;
+
+/**
+ * Created by hadihariri on 10/09/16.
+ */
+public class DropFrame {
+
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            executeMethodA(i);
+            executeMethodB(i);
+        }
+
+    }
+
+    private static void executeMethodB(int i) {
+        int b = i + 20;
+        executeMethodC(b);
+    }
+
+    private static void executeMethodC(int i) {
+        int c = i - 39;
+        executeMethodD(c);
+    }
+
+    private static void executeMethodD(int i) {
+        int d = i + 10;
+        System.out.println(d);
+    }
+
+    private static void executeMethodA(int i) {
+        int a = i * 30;
+        System.out.println(a);
+    }
+}

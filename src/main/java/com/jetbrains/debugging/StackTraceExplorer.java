0516c2705804e6ea4d2a69369e8c8ef72c30a832
hadihariri <mail@hadihariri.com>
2016-09-21 13:46:59 +0200

    Added Stack Trace Sample

---
+++ src/main/java/com/jetbrains/debugging/StackTraceExplorer.java
@@ -0,0 +1,18 @@
+package com.jetbrains.debugging;
+
+public class StackTraceExplorer {
+
+    public static void main(String[] args) {
+
+        executeMethod(10);
+
+    }
+
+    private static void executeMethod(int i) {
+        executeAnotherMethod(30);
+    }
+
+    private static void executeAnotherMethod(int i) {
+        throw new IllegalArgumentException("Oh dear");
+    }
+}

75b7ce0063a8b197665b1bdaff83b721740a422e
hadihariri <mail@hadihariri.com>
2017-03-17 14:29:28 +0100

    Fixed some compilation errors.

---
+++ standard-java/src/main/java/com/jetbrains/refactoring/Java8.java
@@ -0,0 +1,12 @@
+package com.jetbrains.refactoring;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Java8 {
+
+
+    public void print(List<Double> doubles, String usd, String eur, double v) {
+
+    }
+}

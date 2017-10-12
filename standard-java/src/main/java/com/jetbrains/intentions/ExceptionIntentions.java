71b64580a91e2aa19be1fd4922f539f86a7886c0
Trisha Gee <trisha.gee@gmail.com>
2017-10-12 16:38:41 +0200

    Updated project for 2017.3; Updated to Java 9 and Kotlin 1.1.50; Removed Gradle.

--- standard-java/src/main/java/com/jetbrains/intentions/ExceptionIntentions.java
+++ standard-java/src/main/java/com/jetbrains/intentions/ExceptionIntentions.java
@@ -1,8 +1,10 @@
 package com.jetbrains.intentions;
 
-import sun.rmi.transport.Connection;
-
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class ExceptionIntentions {
 

aa7cb52b3b1cbd55894dded91fe9d0542f968e79
Trisha Gee <trisha.gee@gmail.com>
2018-06-26 17:04:29 +0200

    2018.2: IDEA-98499

--- standard-java/src/main/java/com/jetbrains/inspections/Inspections20182.java
+++ standard-java/src/main/java/com/jetbrains/inspections/Inspections20182.java
@@ -1,6 +1,11 @@
 package com.jetbrains.inspections;
 
+import static java.lang.Math.PI;
+
+@SuppressWarnings("unused")
 public class Inspections20182 {
+    private static final int FACTOR = 2;
+
     private void addToExistingCatchBlock() {
         try {
             methodCanThrowExceptionOne();
@@ -11,7 +16,10 @@ public class Inspections20182 {
         }
     }
 
-
+    private void staticImportedConstantsCanLookDifferentToClassConstants() {
+        // see Editor>Colors & Fonts>Java>Class Fields and see two types of constants
+        double x = PI * FACTOR;
+    }
 
 
     private void methodCanThrowExceptionTwo() throws ExceptionTwo {

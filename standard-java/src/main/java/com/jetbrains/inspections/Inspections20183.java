bbe0fc25c476aba25d2d347ad71424099999f300
Trisha Gee <trisha.gee@gmail.com>
2018-12-05 15:29:01 +0100

    Inspections features for 2018.3

---
+++ standard-java/src/main/java/com/jetbrains/inspections/Inspections20183.java
@@ -0,0 +1,18 @@
+package com.jetbrains.inspections;
+
+import static java.lang.Integer.MAX_VALUE;
+import static java.lang.Integer.MIN_VALUE;
+
+@SuppressWarnings("unused")
+public class Inspections20183 {
+
+    @SuppressWarnings({"unused", "unchecked"})
+    public void canReplaceStaticImportWithQualifiers() {
+        System.out.println(MAX_VALUE);
+        System.out.println(MIN_VALUE);
+
+        //TODO: implement all of the rest of the
+        // functionality described in the issues
+        // in YouTrack
+    }
+}

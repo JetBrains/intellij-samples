a4e3df4fe075c14fdb828816240f7dcc5f1c3942
Trisha Gee <trisha.gee@gmail.com>
2018-12-05 15:57:57 +0100

    Java 12 examples

--- standard-java/src/main/java/com/jetbrains/code/Java12.java
+++ standard-java/src/main/java/com/jetbrains/code/Java12.java
@@ -4,49 +4,38 @@ import java.io.IOException;
 
 @SuppressWarnings("unused")
 public class Java12 {
-    String html = `<html>
-    <body>
-		    <p>Hello World with a snjgrn misgnjfrn.</p>
-    </body>
-</html>
-`;
+    String html = "<html>\n" +
+                  "    <body>\n" +
+                  "		    <p>Hello World with a snjgrn misgnjfrn.</p>\n" +
+                  "    </body>\n" +
+                  "</html>\n";
 
-    String splitExample = (`<html>
+    String splitExample = (```<html>
                                  <body>
                                     <p>Split here.</p>
                                  </body>
                               </html>
-                            `).align();
+                            ```).align();
 
-    String script = `function hello() {
-   print('"Hello World"');
-}
+    String script = "function hello() {\n" +
+                    "   print(\'\"Hello World\"\');\n" +
+                    "}\n" +
+                    "\n" +
+                    "hello();\n";
 
-hello();
-`;
-
-    String query = ``SELECT `EMP_ID`, `LAST_NAME` FROM `EMPLOYEE_TB`
-WHERE `CITY` = ‘INDIANAPOLIS'
-ORDER BY `EMP_ID`, `LAST_NAME`;
-``;
+    String query = "SELECT `EMP_ID`, `LAST_NAME` FROM `EMPLOYEE_TB`\n" +
+                   "WHERE `CITY` = ‘INDIANAPOLIS'\n" +
+                   "ORDER BY `EMP_ID`, `LAST_NAME`;\n";
     //note that it doesn't mess with the whitespace when formatting the file
 
-    String pastedMultiLineStringLiteral = "<html>\n" +
-                                          "    <body>\n" +
-                                          "        <p>Text here.</p>\n" +
-                                          "    </body>\n" +
-                                          "</html>";
-    String pastedMultiLineRawStringLiteral = `<html>
-    <body>
-        <p>Text here.</p>
-    </body>
-</html> `;
+    String pastedMultiLineStringLiteral = "";
+    String pastedMultiLineRawStringLiteral = ` `;
 
 
     public static void main(String[] args) throws IOException {
-        Runtime.getRuntime().exec(`"C:\Program Files\foo" bar`); //convert
+        Runtime.getRuntime().exec("\"C:\\Program Files\\foo\" bar"); //convert
 
-        System.out.println("this".matches(`\w\w\w\w`));
+        System.out.println("this".matches("\\w\\w\\w\\w"));
     }
 
 }

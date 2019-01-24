741497525f98d712279f3f60913169fc19684332
Trisha Gee <trisha.gee@gmail.com>
2019-01-24 10:41:10 +0100

    Updating live templates code example

--- standard-java/src/main/java/com/jetbrains/completion/LiveTemplates.java
+++ standard-java/src/main/java/com/jetbrains/completion/LiveTemplates.java
@@ -1,15 +1,15 @@
 package com.jetbrains.completion;
 
-import com.jetbrains.code.Location;
 import com.jetbrains.inspections.entities.Person;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @SuppressWarnings("unused")
 public class LiveTemplates {
 
-    public List<Person> getWorkingEmployees(Location location) {
+    public List<Person> getEmployed(final List<Person> people) {
         List<Person> employees = new ArrayList<>();
 
         return employees;

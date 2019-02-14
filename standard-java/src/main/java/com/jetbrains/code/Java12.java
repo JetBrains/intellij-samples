bfa7e7a04208db6a0ce4bbe2f5fa682e6499baac
Trisha Gee <trisha.gee@gmail.com>
2019-02-14 15:39:57 +0100

    Example code for 2019.1: simple switch

--- standard-java/src/main/java/com/jetbrains/code/Java12.java
+++ standard-java/src/main/java/com/jetbrains/code/Java12.java
@@ -1,41 +1,26 @@
 package com.jetbrains.code;
 
-import java.io.IOException;
-
 @SuppressWarnings("unused")
 public class Java12 {
-    String html = "<html>\n" +
-                  "    <body>\n" +
-                  "		    <p>Hello World with a snjgrn misgnjfrn.</p>\n" +
-                  "    </body>\n" +
-                  "</html>\n";
-
-//    String splitExample = (```<html>
-//                                 <body>
-//                                    <p>Split here.</p>
-//                                 </body>
-//                              </html>
-//                            ```).align();
-
-    String script = "function hello() {\n" +
-                    "   print(\'\"Hello World\"\');\n" +
-                    "}\n" +
-                    "\n" +
-                    "hello();\n";
-
-    String query = "SELECT `EMP_ID`, `LAST_NAME` FROM `EMPLOYEE_TB`\n" +
-                   "WHERE `CITY` = ‘INDIANAPOLIS'\n" +
-                   "ORDER BY `EMP_ID`, `LAST_NAME`;\n";
-    //note that it doesn't mess with the whitespace when formatting the file
-
-    String pastedMultiLineStringLiteral = "";
-//    String pastedMultiLineRawStringLiteral = ` `;
 
 
-    public static void main(String[] args) throws IOException {
-        Runtime.getRuntime().exec("\"C:\\Program Files\\foo\" bar"); //convert
-
-        System.out.println("this".matches("\\w\\w\\w\\w"));
+    private int replaceSimpleSwitch(String category) {
+        int value;
+        switch (category) {
+            case "Low":
+                value = 1;
+                break;
+            case "Medium":
+                value = 2;
+                break;
+            case "High":
+                value = 3;
+                break;
+            default:
+                value = 0;
+                break;
+        }
+        return value;
     }
 
 }

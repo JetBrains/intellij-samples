3b9e175479bb1ed2a074de847cf131f975f76436
Trisha Gee <trisha.gee@gmail.com>
2019-06-24 13:47:29 +0200

    Example for 2019.2 - example code formatting

---
+++ standard-java/src/main/java/com/jetbrains/code/CodeFormatting.java
@@ -0,0 +1,11 @@
+package com.jetbrains.code;
+
+@SuppressWarnings("unused")
+public class CodeFormatting {
+    public void forceIfStatementsToHaveBraces(int someValue) {
+        if (someValue == 2)
+            System.out.println(someValue);
+
+        System.out.println("Value is not two");
+    }
+}

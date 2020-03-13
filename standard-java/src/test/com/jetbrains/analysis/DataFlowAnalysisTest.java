3cab82ae36d613196bde0b1ae537218d87055903
Trisha Gee <trisha.gee@gmail.com>
2020-03-13 15:51:01 +0100

    Adding example for Data Flow Analysis in the debugger

---
+++ standard-java/src/test/com/jetbrains/analysis/DataFlowAnalysisTest.java
@@ -0,0 +1,21 @@
+package com.jetbrains.analysis;
+
+import org.junit.Test;
+
+public class DataFlowAnalysisTest {
+    @Test
+    public void shouldShowDataFlowAnalysisHints() {
+        showTrueWhenLengthIsGreaterThanFour("12345");
+        showTrueWhenLengthIsGreaterThanFour("1234");
+    }
+
+    private void showTrueWhenLengthIsGreaterThanFour(String string) {
+        // breakpoint here
+        var output = 1;
+        // in 2020.1 and above, the line below will show a "true" of "false" hint when debugging
+        if (string.length() > 4) {
+            output++;
+        }
+        System.out.println(output);
+    }
+}

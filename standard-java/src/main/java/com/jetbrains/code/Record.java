78d0bdc5cb03456518591dec4f93b9bd61e59feb
Trisha Gee <trisha.gee@gmail.com>
2020-04-27 13:35:42 +0200

    Adding a validate method to Record so that we can use it when demoing creating constructors

--- standard-java/src/main/java/com/jetbrains/code/Record.java
+++ standard-java/src/main/java/com/jetbrains/code/Record.java
@@ -1,7 +1,12 @@
 package com.jetbrains.code;
 
 // if you want to change a property name you should use refactoring
-public record Record(int id, String name) { }
+public record Record(int id, String name) {
+
+    private void validateMyFields(int id, String name) {
+
+    }
+}
 
 // generate getter does the right thing with the name
 // there is no generate setter

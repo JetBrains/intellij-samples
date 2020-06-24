e317bea1f7ec21a0d55725d4d8c8931397696e90
Trisha Gee <trisha.gee@gmail.com>
2020-06-24 14:59:36 +0200

    Organising the var/lvti examples in a more sensible way

--- standard-java/src/main/java/com/jetbrains/inspections/Java10Inspections.java
+++ standard-java/src/main/java/com/jetbrains/inspections/Java10Inspections.java
@@ -2,15 +2,13 @@ package com.jetbrains.inspections;
 
 import com.jetbrains.inspections.entities.Person;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.Socket;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
-
-import static java.util.stream.Collectors.counting;
-import static java.util.stream.Collectors.groupingBy;
 
 @SuppressWarnings({"unused", "MismatchedQueryAndUpdateOfCollection", "UnnecessaryLocalVariable"})
 public class Java10Inspections {
@@ -22,108 +20,14 @@ public class Java10Inspections {
         return people;
     }
 
+    // Note that we can use the diamond operator and the type is moved right
     private void suggestionProvidedIfTypesMatch() {
-        ArrayList<Person> people = new ArrayList<Person>();
-    }
-
-    private void suggestionNotProvidedIfInterfaceUsed() {
-        List<Person> people = new ArrayList<Person>();
-    }
-
-    private void suggestionProvidedIfDiamondUsedAndTypeMovedToRight() {
         ArrayList<Person> people = new ArrayList<>();
     }
 
-    /* Examples of best practice, as per http://openjdk.java.net/projects/amber/LVTIstyle.html */
-    private void chooseVariableNamesThatProvideUsefulInformation() {
-        List<Person> tmp = getEveryone();
-
-        System.out.println(tmp);
+    private void suggestionNotProvidedIfInterfaceUsed() {
+        List<Person> people = new ArrayList<>();
     }
-
-    private void minimizeTheScopeOfLocalVariables() {
-        ArrayList<Person> list = new ArrayList<Person>();
-
-        list.add(new Person("First"));
-        list.add(new Person("Second"));
-
-        /* There could be a lot of code between the declaration and the use of this variable*/
-
-        for (Person person : list) {
-            System.out.println(person);
-        }
-
-    }
-
-    private void considerVarWhenInitializerProvidesSufficientInformationToReader() throws IOException {
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        BufferedReader reader = Files.newBufferedReader(Paths.get("/"));
-
-        ByteArrayOutputStream stuff = doTheThing();
-    }
-
-    private void takeCareWhenUsingVarWithDiamondOrGenericMethods() {
-        List<String> safeList = List.of("One");
-        List<String> unsafeList = List.of();
-        ArrayList<String> safeArrayList = new ArrayList<String>();
-        ArrayList<String> unsafeArrayList = new ArrayList<>();
-    }
-
-    private Optional<String> useVarToBreakUpChainedOrNestedExpressionsWithLocalVariables(List<String> strings) {
-        return strings.stream()
-                      .collect(groupingBy(s -> s, counting()))
-                      .entrySet()
-                      .stream()
-                      .max(Map.Entry.comparingByValue())
-                      .map(Map.Entry::getKey);
-    }
-
-    void removeMatches(Map<? extends String, ? extends Number> map, int max) {
-        for (Iterator<? extends Map.Entry<? extends String, ? extends Number>> iterator = map.entrySet()
-                                                                                             .iterator(); iterator.hasNext(); ) {
-            Map.Entry<? extends String, ? extends Number> entry = iterator.next();
-            if (max > 0 && matches(entry)) {
-                iterator.remove();
-                max--;
-            }
-        }
-    }
-
-    private String exampleTryWithResources(Socket socket, String charsetName) throws IOException {
-        try (InputStream is = socket.getInputStream();
-             InputStreamReader isr = new InputStreamReader(is, charsetName);
-             BufferedReader buf = new BufferedReader(isr)) {
-            return buf.readLine();
-        }
-    }
-
-    void shouldNotWarnThatInitializerIsRedundant(boolean b) {
-        var x = 0; // 2018.1 warns that initializer is redundant
-        if (b) {
-            x = 5;
-        } else {
-            x = 6;
-        }
-        System.out.println(x);
-    }
-
-    public void shouldNotSuggestRemovingArrayType() {
-        var x = new Integer[]{2, 4, 8};
-    }
-
-    //<editor-fold desc="Helper methods">
-    private boolean matches(Map.Entry<? extends String, ? extends Number> entry) {
-        return false;
-    }
-
-    private ByteArrayOutputStream doTheThing() {
-        return new ByteArrayOutputStream();
-    }
-
-    private List<Person> getEveryone() {
-        return List.of();
-    }
-    //</editor-fold>
 
     //note: this can go to and from the Streams API
     List<String> suggestUsingCollectorsToUnmodifiableList(List<Integer> input) {
@@ -142,19 +46,6 @@ public class Java10Inspections {
                                                  .filter(Objects::nonNull)
                                                  .map(Object::toString)
                                                  .collect(Collectors.toList()));
-    }
-
-    //??
-    List<String> doesNotSuggestUsingCollectorsToUnmodifiableListIfContainsNulls(List<Integer> input) {
-        List<String> list = new ArrayList<>();
-        for (Integer integer : input) {
-            if (integer != null) {
-                list.add(integer.toString());
-            } else {
-                list.add(null);
-            }
-        }
-        return Collections.unmodifiableList(list);
     }
 
     List<String> notAllowedToCollectNullsToUnmodifiableList(List<Integer> input) {
---
+++ standard-java/src/main/java/com/jetbrains/inspections/VarLVTI.java
@@ -0,0 +1,95 @@
+package com.jetbrains.inspections;
+
+import com.jetbrains.inspections.entities.Person;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+
+// Var (Local Variable Type Inference) was introduced in Java 10
+@SuppressWarnings("unused")
+public class VarLVTI {
+    /* Examples of best practice, as per http://openjdk.java.net/projects/amber/LVTIstyle.html */
+    private void chooseVariableNamesThatProvideUsefulInformation() {
+        List<Person> tmp = getEveryone();
+
+        System.out.println(tmp);
+    }
+
+    private void minimizeTheScopeOfLocalVariables() {
+        ArrayList<Person> list = new ArrayList<Person>();
+
+        list.add(new Person("First"));
+        list.add(new Person("Second"));
+
+        /* There could be a lot of code between the declaration and the use of this variable*/
+
+        for (Person person : list) {
+            System.out.println(person);
+        }
+    }
+
+    private void considerVarWhenInitializerProvidesSufficientInformationToReader() throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        BufferedReader reader = Files.newBufferedReader(Paths.get("/"));
+
+        ByteArrayOutputStream stuff = doTheThing();
+    }
+
+    private void takeCareWhenUsingVarWithDiamondOrGenericMethods() {
+        List<String> safeList = List.of("One");
+        List<String> unsafeList = List.of();
+        ArrayList<String> safeArrayList = new ArrayList<String>();
+        ArrayList<String> unsafeArrayList = new ArrayList<>();
+    }
+
+    private Optional<String> varCanBreakUpChainedOrNestedExpressionsWithLocalVariables(List<String> strings) {
+        return strings.stream()
+                      .collect(groupingBy(s -> s, counting()))
+                      .entrySet()
+                      .stream()
+                      .max(Map.Entry.comparingByValue())
+                      .map(Map.Entry::getKey);
+    }
+    /* End of best practice examples */
+
+    void varCanSimplifyGenericTypes(Map<? extends String, ? extends Number> map, int max) {
+        for (Iterator<? extends Map.Entry<? extends String, ? extends Number>> iterator = map.entrySet()
+                                                                                             .iterator();
+             iterator.hasNext(); ) {
+            Map.Entry<? extends String, ? extends Number> entry = iterator.next();
+            if (max > 0 && matches(entry)) {
+                iterator.remove();
+                max--;
+            }
+        }
+    }
+
+    private String varCanSimplifyTryWithResources(Socket socket, String charsetName) throws IOException {
+        try (InputStream is = socket.getInputStream();
+             InputStreamReader isr = new InputStreamReader(is, charsetName);
+             BufferedReader buf = new BufferedReader(isr)) {
+            return buf.readLine();
+        }
+    }
+
+    //<editor-fold desc="Helper methods">
+    private List<Person> getEveryone() {
+        return List.of();
+    }
+
+    private ByteArrayOutputStream doTheThing() {
+        return new ByteArrayOutputStream();
+    }
+
+    private boolean matches(Map.Entry<? extends String, ? extends Number> entry) {
+        return false;
+    }
+    //</editor-fold>
+
+}

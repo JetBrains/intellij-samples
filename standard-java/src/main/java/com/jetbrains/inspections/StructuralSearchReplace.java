82e94d48fcb364bdd067d1bef63b02e0406c901e
Trisha Gee <trisha.gee@gmail.com>
2020-07-07 16:20:39 +0200

    Updated examples for Structural Search

--- standard-java/src/main/java/com/jetbrains/inspections/StructuralSearchReplace.java
+++ standard-java/src/main/java/com/jetbrains/inspections/StructuralSearchReplace.java
@@ -5,8 +5,13 @@ import java.util.logging.Logger;
 
 @SuppressWarnings("unused")
 public class StructuralSearchReplace {
+    private final Logger logger = Logger.getLogger(StructuralSearchReplace.class.getName());
 
-    private Logger logger = Logger.getLogger(StructuralSearchReplace.class.getName());
+    public void logInfoWithinCheck() {
+        if (logger.isLoggable(Level.INFO)) {
+            logger.log(Level.INFO, "A log message");
+        }
+    }
 
     public void logInfo() {
         logger.log(Level.INFO, "A log message");
@@ -22,3 +27,5 @@ public class StructuralSearchReplace {
     }
 
 }
+
+//Search template: logger.log($level$, $message$, $args$); (import existing template of logging without if)

8096d6afe23a62bf8f93b609aafce0bc1d0bdecd
Trisha Gee <trisha.gee@gmail.com>
2020-07-09 16:11:45 +0200

    JDK 15 demo examples

--- standard-java/src/main/java/com/jetbrains/code/Record.java
+++ standard-java/src/main/java/com/jetbrains/code/Record.java
@@ -1,22 +1,15 @@
 package com.jetbrains.code;
 
 // if you want to change a property name you should use refactoring
+// Generate a constructor. Note: non-public constructors supported (Java 15)
+// Generate an accessor. Note: annotations supported (Java 15)
 record Record(int id, String name) {
-    Record(int id, String name) { // non-public constructors supported
-        this.id = id;
-        this.name = name;
-    }
 
     private void validateMyFields(int id, String name) {
 
     }
 
-    @Override // support added in Java 15
-    public String name() {
-        return name;
-    }
 }
-
 // generate getter does the right thing with the name
 // there is no generate setter
 

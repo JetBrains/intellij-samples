a79a88076cfc6186b0d0f9cd1c6e23cd5f7224e7
Trisha Gee <trisha.gee@gmail.com>
2020-10-26 16:15:03 +0100

    Updates to the LVTI examples to make it a bit easier to demo. Also corrected an example that IntelliJ IDEA takes care of automatically now

--- standard-java/src/main/java/com/jetbrains/inspections/VarLVTI.java
+++ standard-java/src/main/java/com/jetbrains/inspections/VarLVTI.java
@@ -11,17 +11,45 @@ import java.util.*;
 import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
 
-// Var (Local Variable Type Inference) was introduced in Java 10
+// var (Local Variable Type Inference) was introduced in Java 10
+// var can be automatically applied across the codebase, but should be done on a case-by-case basis
 @SuppressWarnings("unused")
 public class VarLVTI {
     /* Examples of best practice, as per http://openjdk.java.net/projects/amber/LVTIstyle.html */
+    private void considerVarWhenInitializerProvidesSufficientInformationToReader() throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        BufferedReader reader = Files.newBufferedReader(Paths.get("/"));
+
+        ByteArrayOutputStream stuff = doTheThing();
+    }
+
+    private void takeCareWhenUsingVarWithDiamondOrGenericMethods() {
+        List<String> safeList = List.of("One");
+        List<String> unsafeList = List.of();
+
+        ArrayList<String> intelliJIDEAMovesTheTypeToTheRight = new ArrayList<>();
+    }
+
+    private Optional<String> varCanBreakUpChainedOrNestedExpressionsWithLocalVariables(List<String> strings) {
+        return strings.stream()
+                      .collect(groupingBy(s -> s, counting()))
+                      .entrySet()
+                      .stream()
+                      .max(Map.Entry.comparingByValue())
+                      .map(Map.Entry::getKey);
+    }
+
+    // Developer knowledge required
     private void chooseVariableNamesThatProvideUsefulInformation() {
         List<Person> tmp = getEveryone();
 
         System.out.println(tmp);
     }
 
+    // Developer knowledge required
     private void minimizeTheScopeOfLocalVariables() {
+        // imagine if someone changed the type of this from ArrayList to HashSet, it would be hard to see the
+        // implication if the use of the code is much further away
         ArrayList<Person> list = new ArrayList<Person>();
 
         list.add(new Person("First"));
@@ -34,28 +62,6 @@ public class VarLVTI {
         }
     }
 
-    private void considerVarWhenInitializerProvidesSufficientInformationToReader() throws IOException {
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        BufferedReader reader = Files.newBufferedReader(Paths.get("/"));
-
-        ByteArrayOutputStream stuff = doTheThing();
-    }
-
-    private void takeCareWhenUsingVarWithDiamondOrGenericMethods() {
-        List<String> safeList = List.of("One");
-        List<String> unsafeList = List.of();
-        ArrayList<String> safeArrayList = new ArrayList<String>();
-        ArrayList<String> unsafeArrayList = new ArrayList<>();
-    }
-
-    private Optional<String> varCanBreakUpChainedOrNestedExpressionsWithLocalVariables(List<String> strings) {
-        return strings.stream()
-                      .collect(groupingBy(s -> s, counting()))
-                      .entrySet()
-                      .stream()
-                      .max(Map.Entry.comparingByValue())
-                      .map(Map.Entry::getKey);
-    }
     /* End of best practice examples */
 
     void varCanSimplifyGenericTypes(Map<? extends String, ? extends Number> map, int max) {

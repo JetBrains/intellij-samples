4707963244209809d59a00586008c3c1d79d5491
Trisha Gee <trisha.gee@gmail.com>
2020-10-26 18:35:54 +0100

    Minor changes to make it easier to demo Text Blocks

--- standard-java/src/main/java/com/jetbrains/code/TextBlocks.java
+++ standard-java/src/main/java/com/jetbrains/code/TextBlocks.java
@@ -8,31 +8,31 @@ public class TextBlocks {
                   "    </body>\n" +
                   "</html>\n";
 
+    String query = "SELECT id, last_name FROM employee\n" +
+                   "WHERE city = 'London'\n" +
+                   "ORDER BY id, last_name;\n";
+
     String script = "function hello() {\n" +
                     "   print(\"Hello World\");\n" +
                     "}\n" +
                     "\n" +
                     "hello();\n";
 
-    String query = "SELECT id, last_name FROM employee\n" +
-                   "WHERE city = 'London'\n" +
-                   "ORDER BY id, last_name;\n";
-
     String json = "{\n" +
-            "      \"openStreetMapId\": 2852153005,\n" +
-            "      \"location\": {\n" +
-            "        \"coordinates\": [\n" +
-            "          \"-73.616748\",\n" +
-            "          \"45.4699795\"\n" +
-            "        ],\n" +
-            "        \"type\": \"Point\"\n" +
-            "      },\n" +
-            "      \"amenity\": \"cafe\",\n" +
-            "      \"cuisine\": \"coffee_shop\",\n" +
-            "      \"internet_access\": \"yes\",\n" +
-            "      \"name\": \"Chimera\",\n" +
-            "      \"smoking\": \"no\"\n" +
-            "    }";
+                  "      \"openStreetMapId\": 2852153005,\n" +
+                  "      \"location\": {\n" +
+                  "        \"coordinates\": [\n" +
+                  "          \"-73.616748\",\n" +
+                  "          \"45.4699795\"\n" +
+                  "        ],\n" +
+                  "        \"type\": \"Point\"\n" +
+                  "      },\n" +
+                  "      \"amenity\": \"cafe\",\n" +
+                  "      \"cuisine\": \"coffee_shop\",\n" +
+                  "      \"internet_access\": \"yes\",\n" +
+                  "      \"name\": \"Chimera\",\n" +
+                  "      \"smoking\": \"no\"\n" +
+                  "    }";
 
     //note that it doesn't mess with the whitespace when formatting the file
 

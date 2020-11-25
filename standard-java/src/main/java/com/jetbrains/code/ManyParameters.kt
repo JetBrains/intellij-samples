e8ce7c6c386356f7ed00bd2ec89fa7f15eda8e35
Trisha <trisha.gee@gmail.com>
2020-11-25 13:53:39 +0100

    Removing warnings from ManyParameters.kt

--- standard-java/src/main/java/com/jetbrains/code/ManyParameters.kt
+++ standard-java/src/main/java/com/jetbrains/code/ManyParameters.kt
@@ -1,14 +1,15 @@
+@file:Suppress("unused")
+
 package com.jetbrains.code
 
 import com.jetbrains.entity.Order
 import java.time.LocalDateTime
 
-@Suppress("unused")
 // Use Alt+Enter and select "Put parameters on separate lines"
 data class ManyParameters(val firstName: String, val id: Int, val lastName: String, val orders: List<Order>) {
 
     fun formatOrders(sellerId: Int, fromDate: LocalDateTime, toDate: LocalDateTime, handler: (Order) -> String) {
-
+        println("sellerId = [${sellerId}], fromDate = [${fromDate}], toDate = [${toDate}], handler = [${handler}]")
         // TODO: format all the orders
     }
 }

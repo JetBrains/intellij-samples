d6ed8e4bfd7439875f79faf754ce875ea7c8e0a6
Trisha Gee <trisha.gee@gmail.com>
2020-12-09 12:58:20 +0100

    Added Javadoc to a record to see what it looked like

--- standard-java/src/main/java/com/jetbrains/code/Record.java
+++ standard-java/src/main/java/com/jetbrains/code/Record.java
@@ -1,9 +1,18 @@
 package com.jetbrains.code;
 
-// if you want to change a property name you should use refactoring
-// - Generate a constructor. Note: non-public constructors supported (Java 15)
-// - Generate an accessor. Note: annotations supported (Java 15)
+/**
+ * Simple example of a record.
+ *
+ * If you want to change a property name you should use refactoring
+ *
+ * @param id unique identifier
+ * @param name the descriptive name
+ */
 record Record(int id, String name) {
+    // - Generate a constructor. Note: non-public constructors supported (Java 15)
+    // - Generate an accessor. Note: annotations supported (Java 15)
+    // - Generate getter does the right thing with the name
+    // - There is no generate setter
 
     //<editor-fold desc="Helper methods">
     @SuppressWarnings("unused")
@@ -13,6 +22,4 @@ record Record(int id, String name) {
     //</editor-fold>
 
 }
-// generate getter does the right thing with the name
-// there is no generate setter
 

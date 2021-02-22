57d5b64e12008f0236e4724f3a5d3855eeea62bd
Trisha Gee <trisha.gee@gmail.com>
2021-02-22 18:53:33 +0100

    [EVAN-9314] Simplify extract method

--- standard-java/src/main/java/com/jetbrains/refactoring/ExtractMethod.java
+++ standard-java/src/main/java/com/jetbrains/refactoring/ExtractMethod.java
@@ -1,32 +1,31 @@
 package com.jetbrains.refactoring;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings("unused")
 public class ExtractMethod {
-    public static void main(String[] args) {
-        String s = "How to extract a method: ";
+    void main() {
+        System.out.println("How to extract a method: ");
 
         String hotkey = "Ctrl/Cmd+Alt+M";
 
-        String[] steps = new String[5];
-        steps[0] = "First select a block of code";
-        steps[1] = "Then press " + hotkey;
-        steps[2] = "Give the method a name";
-        steps[3] = "Assign it a visibility";
-        steps[4] = "Apply the refactoring";
-
-        System.out.println(s);
+        List<String> steps = new ArrayList<>();
+        steps.add("First select a block of code");
+        steps.add("Then press " + hotkey);
+        steps.add("Give the method a name");
+        steps.add("Assign it a visibility");
+        steps.add("Apply the refactoring");
 
         //Extract this loop
-        for(String step : steps) {
+        for (String step : steps) {
             System.out.println(step);
         }
     }
 
-    @SuppressWarnings("unused")
-    public static class StepPrinter {
-        public void printTheSteps(String... theSteps) {
-            for(String step : theSteps) {
-                System.out.println(step);
-            }
+    public void printList(List<String> values) {
+        for (String value : values) {
+            System.out.println(value);
         }
     }
 }

670bdc6861a7edffa77d3c2b61bcd6af65bcf9e0
Trisha Gee <trisha.gee@gmail.com>
2021-02-22 16:01:02 +0100

    [EVAN-9314] Basic example to compile, run and debug

---
+++ standard-java/src/main/java/com/jetbrains/running/Hello.java
@@ -0,0 +1,8 @@
+package com.jetbrains.running;
+
+public class Hello {
+    public static void main(String[] args) {
+        String message = "Happy 20th Birthday IntelliJ IDEA!";
+        System.out.println(message);
+    }
+}

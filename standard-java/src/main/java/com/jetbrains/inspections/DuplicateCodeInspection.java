3b859a6f2ca080fa1d9e32a010ff4ee5d5d5c5d4
Trisha Gee <trisha.gee@gmail.com>
2021-02-22 18:30:47 +0100

    [EVAN-9314] Trying to simplify duplicate code example

--- standard-java/src/main/java/com/jetbrains/inspections/DuplicateCodeInspection.java
+++ standard-java/src/main/java/com/jetbrains/inspections/DuplicateCodeInspection.java
@@ -1,36 +1,23 @@
 package com.jetbrains.inspections;
 
-import java.io.FileWriter;
-import java.io.IOException;
-
 @SuppressWarnings("unused")
 public class DuplicateCodeInspection {
     public static void main(String[] args) {
-        StringBuilder stringBuilder = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         for (String arg : args) {
-            stringBuilder.append(arg).append(", ");
+            sb.append(arg).append(", ");
         }
 
-        try {
-            FileWriter fw = new FileWriter("temp");
-            fw.write(stringBuilder.toString());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        System.out.println(sb);
     }
 
     // Note that if variable names are different it still counts as duplicate code
-    public void duplicate(String[] params) {
+    public StringBuilder duplicate(String[] params) {
         StringBuilder stringBuilder = new StringBuilder();
         for (String param : params) {
             stringBuilder.append(param).append(", ");
         }
 
-        try {
-            FileWriter writer = new FileWriter("temp");
-            writer.write(stringBuilder.toString());
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+        return stringBuilder;
     }
 }

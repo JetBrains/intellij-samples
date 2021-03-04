c44404323acdac1a0ff1f4dbc00b03b1aaf208a7
Trisha Gee <trisha.gee@gmail.com>
2021-03-04 16:52:23 +0100

    [EVAN-9273] 2021.1 Examples. The model should not be in the repo.

--- standard-java/src/main/java/com/jetbrains/domain/repository/UserRepository.java
+++ standard-java/src/main/java/com/jetbrains/domain/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.jetbrains.domain.repository;
 
 public class UserRepository {
     // does data access. This should only access the database.
-    // try adding a dependency on com.jetbrains.domain.ui.UserModel, and see the dependencies inspection give a warning
+    // try adding a dependency on com.jetbrains.domain.service.UserService, and see the dependencies inspection give a warning
 
-    com.jetbrains.domain.ui.UserModel userModel;
 }

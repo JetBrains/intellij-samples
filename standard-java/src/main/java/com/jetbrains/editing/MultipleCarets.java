74a06782bef100e36c96bd5db0958663ec42be86
Helen Scott <helen.scott77@gmail.com>
2021-03-08 14:40:28 +0000

    Adding classes for multiple carets and javadoc formatting

---
+++ standard-java/src/main/java/com/jetbrains/editing/MultipleCarets.java
@@ -0,0 +1,16 @@
+package com.jetbrains.editing;
+
+public class MultipleCarets {
+
+    private final String oak = "Oak";
+    private final String sycamore = "Sycamore";
+    private final String pine = "Pine";
+
+    private final String lion = "Lives in Africa and eats meat";
+    private final String wolf = "Lives in Europe and eats meat";
+    private final String stoat = "Lives in the United Kingdom and eats meat";
+
+    int[] input = {1346, 1213, 1933, 834, 1598, 1191, 1979, 1756, 1216, 1820, 1792, 1537, 1994, 1337, 1883, 1519};
+
+
+}

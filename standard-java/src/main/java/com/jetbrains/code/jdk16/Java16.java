1d85347f0a70c626ab1b89c27e12bab8a7af35f5
Trisha Gee <trisha.gee@gmail.com>
2021-03-11 13:28:18 +0100

    [EVAN-9452] Better example for Wrapper constructor

--- standard-java/src/main/java/com/jetbrains/code/jdk16/Java16.java
+++ standard-java/src/main/java/com/jetbrains/code/jdk16/Java16.java
@@ -4,6 +4,6 @@ package com.jetbrains.code.jdk16;
 public class Java16 {
     void shouldWarnForWrapperConstructors() {
         // these were deprecated in Java 9, but now for #JDK16 marked for removal (JEP 390)
-        Double aDouble = new Double(1);
+        Double aDouble = new Double(1.0);
     }
 }

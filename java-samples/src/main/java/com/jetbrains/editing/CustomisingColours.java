49144d7bab60e606ac7f1392392f4936b9e9093d
Trisha Gee <trisha.gee@gmail.com>
2021-07-01 17:13:54 +0200

    [EVAN-9676] Examples for customising colours based on visibility.

--- java-samples/src/main/java/com/jetbrains/editing/CustomisingColours.java
+++ java-samples/src/main/java/com/jetbrains/editing/CustomisingColours.java
@@ -5,11 +5,35 @@ import static java.lang.Math.PI;
 @SuppressWarnings("unused")
 public class CustomisingColours {
     private static final int FACTOR = 2;
+    private int privateField = 0;
+    protected int protectedField = 0;
 
     private void staticImportedConstantsCanLookDifferentToClassConstants() {
         // see Editor>Colors & Fonts>Java>Class Fields and see two types of constants
         double x = PI * FACTOR;
     }
 
+    public void membersCanBeColouredAccordingToVisibility() {
+        // change settings in Preferences/Settings | Editor | Color Scheme by unfolding the Visibility node.
+        publicMethod();
+        protectedMethod();
+        protectedField++;
+        packagePrivateMethod();
+        privateMethod();
+        privateField++;
+    }
+
+
+    public void publicMethod() {
+    }
+
+    protected void protectedMethod() {
+    }
+
+    void packagePrivateMethod() {
+    }
+
+    private void privateMethod() {
+    }
 
 }

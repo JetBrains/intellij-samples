c3e841e930c2b7f4644937ce46da092f7ac31cbe
Trisha Gee <trisha.gee@gmail.com>
2021-09-02 10:22:43 +0200

    IntelliJ IDEA supports pattern matching for switch (preview) - JEP 406.

--- java-samples/src/main/java/com/jetbrains/code/jdk17/PatternMatchingSwitch.java
+++ java-samples/src/main/java/com/jetbrains/code/jdk17/PatternMatchingSwitch.java
@@ -14,4 +14,14 @@ public class PatternMatchingSwitch {
         }
         return formatted;
     }
+
+    static String formatterPatternSwitch(Object o) {
+        return switch (o) {
+            case Integer i -> String.format("int %d", i);
+            case Long l -> String.format("long %d", l);
+            case Double d -> String.format("double %f", d);
+            case String s -> String.format("String %s", s);
+            default -> o.toString();
+        };
+    }
 }

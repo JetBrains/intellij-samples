bb2bbaea9fb3202d49fd59395426cfa4c0125b7b
havyabhogadi <havyabhogadi@gmail.com>
2022-04-02 20:22:19 -0700

    welcome message added

--- java-samples/src/main/java/com/jetbrains/code/jdk17/Hello.java
+++ java-samples/src/main/java/com/jetbrains/code/jdk17/Hello.java
@@ -3,5 +3,6 @@ package com.jetbrains.code.jdk17;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello!");
+        System.out.println("welcome");
     }
 }

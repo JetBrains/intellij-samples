33efec66d545d92d74048e892b94cc277710430e
Helen Scott <helen.scott77@gmail.com>
2022-05-23 08:07:21 +0100

    example for pattern matching switch statements

---
+++ java-samples/src/main/java/com/jetbrains/code/jdk17/PatternMatchingSwitch.java
@@ -0,0 +1,27 @@
+package com.jetbrains.code.jdk17;
+
+@SuppressWarnings("unused")
+public class PatternMatchingSwitch {
+
+    static class AirPollution {
+        public int getAQI() {
+            return 100;
+        }
+
+        public int getDamage(Object o) {
+            return 0;
+        }
+    }
+
+    public static class Discrimination {
+        public int damagingGenerations() {
+            return 2000;
+        }
+    }
+    public static class Deforestation {
+        public int getTreeDamage() {
+            return 300;
+        }
+    }
+
+}

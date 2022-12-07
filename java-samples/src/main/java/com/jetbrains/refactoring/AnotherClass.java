5f66ce68b55bad379edd913f86804ed17e18f714
marit.van.dijk <mlvandijk@gmail.com>
2022-12-07 12:09:59 +0100

    Add Refactor ExtractVariable

---
+++ java-samples/src/main/java/com/jetbrains/refactoring/AnotherClass.java
@@ -0,0 +1,7 @@
+package com.jetbrains.refactoring;
+
+public class AnotherClass {
+    public int intValue() {
+        return 1;
+    }
+}
--- java-samples/src/main/java/com/jetbrains/refactoring/ExtractVariable.java
+++ java-samples/src/main/java/com/jetbrains/refactoring/ExtractVariable.java
@@ -5,16 +5,10 @@ public class ExtractVariable {
 
     // The Extract Variable refactoring wraps a selected expression into a variable.
     // It adds a new variable declaration and uses the expression as an initializer.
-    //  Select an expression and press ⌥⌘V (on Mac) or Ctrl+Alt+V (on Windows/Linux)
+    //  Select an expression and press ⌥⌘V (on Mac) or Ctrl+Alt+V (on Windows/Linux).
     public void method() {
         int a = 1;
         int b = a + anotherClass.intValue();
         int c = b + anotherClass.intValue();
-    }
-}
-
-class AnotherClass {
-    public int intValue() {
-        return 1;
     }
 }

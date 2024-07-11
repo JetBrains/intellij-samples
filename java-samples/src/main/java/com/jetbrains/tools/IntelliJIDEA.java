d8e82e7d977ad9770fe27af277e6d8e714c320f9
marit.van.dijk <mlvandijk@gmail.com>
2024-07-11 12:48:47 +0200

    Update Strings

--- java-samples/src/main/java/com/jetbrains/tools/IntelliJIDEA.java
+++ java-samples/src/main/java/com/jetbrains/tools/IntelliJIDEA.java
@@ -1,14 +1,14 @@
 package com.jetbrains.tools;
 
 public class IntelliJIDEA {
-    private final static String description = "Java, JVM and polyglot IDE";
+    private final static String description = "The Leading Java and Kotlin IDE";
 
     public static void main(String[] args) {
         var message = "There's so much more to do than simply say 'Hello World!'";
         System.out.println(message);
 
         var developer = new Developer(ProductivityLevel.HIGH);
-        developer.developWithPleasure("The Drive to Develop");
+        developer.developWithPleasure("Make it happen. With code.");
     }
 
     record Developer(ProductivityLevel level) {

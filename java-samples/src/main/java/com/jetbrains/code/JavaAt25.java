8d93f73bd3c99a1de820d200e642fe39a342c8c7
Marit van Dijk <marit.van.dijk@jetbrains.com>
2024-09-28 06:54:56 +0200

    Add text for tip on view modes (#31)
    * Add text for tip on view modes

--- java-samples/src/main/java/com/jetbrains/code/JavaAt25.java
+++ java-samples/src/main/java/com/jetbrains/code/JavaAt25.java
@@ -18,6 +18,8 @@ import java.util.stream.Collectors;
 // 2. Enter Distraction free mode
 // 3. Enter Full screen mode
 // 4. Enter Zen mode
+// 5. Toggle viewing modes with
+// Quick Switch Scheme ⌃`(macOS), \` (Windows/Linux)
 
 public class JavaAt25 {
     private void weLoveCollections() {

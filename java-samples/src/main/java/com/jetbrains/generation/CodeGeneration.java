40134529de70ad5ba96c4682ea9d16d8573e9387
Marit van Dijk <marit.van.dijk@jetbrains.com>
2025-11-26 15:03:58 +0100

    Add new example for generate equals() and hashCode() (#32)

---
+++ java-samples/src/main/java/com/jetbrains/generation/CodeGeneration.java
@@ -0,0 +1,26 @@
+package com.jetbrains.generation;
+
+import java.util.List;
+
+public class CodeGeneration {
+
+    int age;
+    List<String> names;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<String> getNames() {
+        return names;
+    }
+
+    public void setNames(List<String> names) {
+        this.names = names;
+    }
+
+}
